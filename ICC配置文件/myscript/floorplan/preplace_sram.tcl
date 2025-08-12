896b2d3564c79fb6d46aa3e98acbd6da301c9c7c
ZiAng LIN <linziang100@foxmail.com>
2025-08-12 16:03:20 +0800

    .

--- ICC配置文件/myscript/floorplan/preplace_sram.tcl
+++ ICC配置文件/myscript/floorplan/preplace_sram.tcl
@@ -1,11 +1,22 @@
-# TO DO: command for preplace sram
-# 脚本：preplace_sram.tcl 
-# unfix and unplace SRAM 
-set_undoable_attribute [get_cells -all x_data_sram/i_sram_block ] is_fixed {0} 
-set_undoable_attribute [get_cells -all x_data_sram/i_sram_block ] is_placed {0} 
-# 将指令SRAM放置在指定坐标上，需要在setup中修改对应的参数以调整位置 
-move_objects -x $DATA_SRAM_X -y $DATA_SRAM_Y x_data_sram/i_sram_block 
-# 连接vdd，vss 
-preroute_instances -connect_instances specified -cells x_data_sram/i_sram_block-select_net_by_type pg -target_directions four_sides -skip_bottom_side -primary_routing_layer specified -specified_horizontal_layer M3 -specified_vertical_layer M4 
-# 宏模块之间和宏模块-边界之间不要放置标准单元，预留足够的布线通道  
+# Wenxun: to do
+# un-fix and un-place sram
+set_undoable_attribute [get_cells -all x_data_sram/i_sram_block ] is_fixed {0}
+set_undoable_attribute [get_cells -all x_data_sram/i_sram_block ] is_placed {0}
+set_undoable_attribute [get_cells -all x_isram_ahbl/i_sram_block] is_fixed {0}
+set_undoable_attribute [get_cells -all x_isram_ahbl/i_sram_block] is_placed {0}
+
+# move
+# Wenxun : to do
+move_objects -x $DATA_SRAM_X -y $DATA_SRAM_Y x_data_sram/i_sram_block
+move_objects -x $INST_SRAM_X -y $INST_SRAM_Y x_isram_ahbl/i_sram_block
+
+# connect vdd,vss
+preroute_instances -connect_instances specified -cells x_data_sram/i_sram_block -select_net_by_type pg -target_directions four_sides -skip_bottom_side -primary_routing_layer specified -specified_horizontal_layer M3 -specified_vertical_layer M4
+
+preroute_instances -connect_instances specified -cells x_isram_ahbl/i_sram_block -select_net_by_type pg -target_directions four_sides -skip_bottom_side -primary_routing_layer specified -specified_horizontal_layer M3 -specified_vertical_layer M4
+
+# sram padding
 set_keepout_margin -type hard -outer {$DATA_SRAM_KEEPOUT_WIDTH $DATA_SRAM_KEEPOUT_WIDTH $DATA_SRAM_KEEPOUT_WIDTH $DATA_SRAM_KEEPOUT_WIDTH} x_data_sram/i_sram_block
+
+set_keepout_margin -type hard -outer {$INST_SRAM_KEEPOUT_WIDTH $INST_SRAM_KEEPOUT_WIDTH $INST_SRAM_KEEPOUT_WIDTH $INST_SRAM_KEEPOUT_WIDTH} x_isram_ahbl/i_sram_block
+
---
+++ ICC配置文件/myscript/floorplan/vdd_vss_rings_straps.tcl
@@ -0,0 +1,12 @@
+# Wenxun: disable this rule check to ensure rings and connection will be made
+set_preroute_drc_strategy -ignore_discrete_metal_width_rule
+
+create_rectangular_rings -around core -nets GND_SOC -left_segment_layer M4 -right_segment_layer M4 -bottom_segment_layer M3 -top_segment_layer M3 -left_segment_width $POWER_RING_WIDTH -right_segment_width $POWER_RING_WIDTH -bottom_segment_width $POWER_RING_WIDTH -top_segment_width $POWER_RING_WIDTH -left_offset $POWER_RING_PITCH -right_offset $POWER_RING_PITCH -bottom_offset $POWER_RING_PITCH -top_offset $POWER_RING_PITCH -offsets absolute
+
+create_rectangular_rings -around core -nets VDD_SOC -left_segment_layer M4 -right_segment_layer M4 -bottom_segment_layer M3 -top_segment_layer M3 -left_segment_width $POWER_RING_WIDTH -right_segment_width $POWER_RING_WIDTH -bottom_segment_width $POWER_RING_WIDTH -top_segment_width $POWER_RING_WIDTH -left_offset [expr 2*$POWER_RING_PITCH + $POWER_RING_WIDTH] -right_offset [expr 2*$POWER_RING_PITCH + $POWER_RING_WIDTH] -bottom_offset [expr 2*$POWER_RING_PITCH + $POWER_RING_WIDTH] -top_offset [expr 2*$POWER_RING_PITCH + $POWER_RING_WIDTH] -offsets absolute
+
+# TO DO: Create your Ring and Straps, must exists rings and straps both.
+# e.g. create_power_straps -nets {xxx} -direction vertical -start_at [$POS] -width 5 -num_placement_strap 1 -start_low_ends coordinate -start_low_ends_coordinate [$POS1] -start_high_ends coordinate -start_high_ends_coordinate [$POS2] -layer M4 -extend_low_ends off -extend_high_ends off
+create_power_straps -nets VDD_SOC -direction vertical -start_at $POS_START -width 5 -num_placement_strap 1 -start_low_ends coordinate -start_low_ends_coordinate $POS_L -start_high_ends coordinate -start_high_ends_coordinate $POS_H -layer M4 -extend_low_ends off -extend_high_ends off
+
+create_power_straps -nets GND_SOC -direction vertical -start_at [$POS_START -width 5 -num_placement_strap 1 -start_low_ends coordinate -start_low_ends_coordinate $POS_L -start_high_ends coordinate -start_high_ends_coordinate $POS_H -layer M4 -extend_low_ends off -extend_high_ends off
